93577f3479de68843045da011bd2720b4462827d
ZionJiam <52207958+ZionJiam@users.noreply.github.com>
2025-07-08 17:30:51 +0800

    update meta tags

--- binarytree/app/layout.tsx
+++ binarytree/app/layout.tsx
@@ -12,6 +12,22 @@ export const metadata: Metadata = {
   icons: {
     icon: '/favicon.ico',
   },
+
+  openGraph: {
+    title: 'Binarytree',
+    description: "Showcase your technical prowess, Impress Recruiters, Get more interviews, Land your dream job",
+    url: 'https://www.binarytree.me', // Your website's full URL
+    siteName: 'Binarytree',
+    images: [
+      {
+        url: '/binarytree-logo.png', // Path to your image in the public folder
+        width: 1200,
+        height: 630,
+      },
+    ],
+    locale: 'en_US',
+    type: 'website',
+  },
 };
 
 
@@ -24,7 +40,7 @@ export default function RootLayout({
     <html lang="en">
       <body>
         <ReactQueryProvider>
-              {children}
+          {children}
         </ReactQueryProvider>
 
       </body>
--- binarytree/app/main/layout.tsx
+++ binarytree/app/main/layout.tsx
@@ -1,16 +1,16 @@
 // app/(main)/layout.tsx
-import {Header} from '@/app/main/Header';
-import {Footer} from '@/app/main/Footer';
+import { Header } from '@/app/main/Header';
+import { Footer } from '@/app/main/Footer';
 import { Toaster } from "sonner";
 
 
 export default function MainLayout({ children }: { children: React.ReactNode }) {
   return (
     <>
-        <Header />
-            <main>{children}</main>
-            <Toaster richColors position="bottom-center" />
-        <Footer />
+      <Header />
+      <main>{children}</main>
+      <Toaster richColors position="bottom-center" />
+      <Footer />
     </>
 
   );
